0866b62e764d6496a646c5efae799dcef73286d1
Stephen Brennan <stephen@brennan.io>
2025-01-06 23:35:35 -0800

    sh2: use dynamic array and character buffer
    Signed-off-by: Stephen Brennan <stephen@brennan.io>

--- src/main.c
+++ src/main.c
@@ -9,6 +9,8 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+#include "sc-collections.h"
+
 static int builtin_cd(char **args);
 static int builtin_help(char **args);
 static int builtin_exit(char **args);
@@ -142,91 +144,38 @@ static int run_command(char **args)
 
 static char *read_line(void)
 {
-#ifdef SH2_USE_STD_GETLINE
-	char *line = NULL;
-	ssize_t bufsize = 0; // have getline allocate a buffer for us
-	if (getline(&line, &bufsize, stdin) == -1) {
-		if (feof(stdin)) {
-			exit(EXIT_SUCCESS); // We received an EOF
-		} else {
-			perror("lsh: getline\n");
-			exit(EXIT_FAILURE);
-		}
-	}
-	return line;
-#else
-#define SH2_RL_BUFSIZE 1024
-	int bufsize = SH2_RL_BUFSIZE;
-	int position = 0;
-	char *buffer = malloc(sizeof(char) * bufsize);
-	int c;
-
-	if (!buffer) {
-		fprintf(stderr, "lsh: allocation error\n");
-		exit(EXIT_FAILURE);
-	}
+	struct sc_charbuf cb;
+	sc_cb_init(&cb, 128);
 
 	while (1) {
-		// Read a character
-		c = getchar();
+		int c = getchar();
 
 		if (c == EOF) {
 			exit(EXIT_SUCCESS);
 		} else if (c == '\n') {
-			buffer[position] = '\0';
-			return buffer;
+			sc_cb_trim(&cb);
+			return cb.buf;
 		} else {
-			buffer[position] = c;
-		}
-		position++;
-
-		// If we have exceeded the buffer, reallocate.
-		if (position >= bufsize) {
-			bufsize += SH2_RL_BUFSIZE;
-			buffer = realloc(buffer, bufsize);
-			if (!buffer) {
-				fprintf(stderr, "lsh: allocation error\n");
-				exit(EXIT_FAILURE);
-			}
+			sc_cb_append(&cb, (char) c);
 		}
 	}
-#endif
 }
 
-#define SH2_TOK_BUFSIZE 64
 #define SH2_TOK_DELIM   " \t\r\n\a"
 
 static char **split_line(char *line)
 {
-	int bufsize = SH2_TOK_BUFSIZE, position = 0;
-	char **tokens = malloc(bufsize * sizeof(char *));
-	char *token, **tokens_backup;
-
-	if (!tokens) {
-		fprintf(stderr, "lsh: allocation error\n");
-		exit(EXIT_FAILURE);
-	}
+	struct sc_array arr;
+	sc_arr_init(&arr, char *, 16);
+	char *token;
 
 	token = strtok(line, SH2_TOK_DELIM);
 	while (token != NULL) {
-		tokens[position] = token;
-		position++;
-
-		if (position >= bufsize) {
-			bufsize += SH2_TOK_BUFSIZE;
-			tokens_backup = tokens;
-			tokens = realloc(tokens, bufsize * sizeof(char *));
-			if (!tokens) {
-				free(tokens_backup);
-				fprintf(stderr, "lsh: allocation error\n");
-				exit(EXIT_FAILURE);
-			}
-		}
-
+		sc_arr_append(&arr, char *, token);
 		token = strtok(NULL, SH2_TOK_DELIM);
 	}
-	tokens[position] = NULL;
-	return tokens;
+	sc_arr_append(&arr, char *, NULL);
+	return arr.arr;
 }
 
 int main(int argc, char **argv)
